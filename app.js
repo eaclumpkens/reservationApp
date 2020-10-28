b1fcf74e8579f5f277515f128f5d5beac5343df7
eaclumpkens <eaclumpkens@gmail.com>
2020-10-27 20:48:48 -0400

    routename functionality

--- app.js
+++ app.js
@@ -7,25 +7,53 @@ const PORT = 8000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-const allReservations = [];
+let allReservations = [
+    {
+        name: "John",
+        phone: "111-111-1111",
+        email: "email@gmail.com",
+    }
+];
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/create-reservation", function(req, res) {
-    res.sendFile(path.join(__dirname, "reservation.html"));
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/api/reservations", function(req, res){
-    var reservation = req.body
-    allReservations.push(reservation);
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
 
-    res.json(reservations);
+app.get("/api/reservations", function(req, res) {
+    return res.json(allReservations);
+})
+
+app.get("/api/reservations/:reservation", function(req, res) {
+    var selected = req.params.reservation;
+    console.log(selected);
+
+    for (var i = 0; i < allReservations.length; i++) {
+        if (selected === allReservations[i].routeName) {
+            return res.json(allReservations[i]);
+        }
+    }
+
+    return res.json(false);
+});
+
+app.post("/api/reservations", function(req, res){
+    var data = req.body
+    data.routeName = data.name.replace(/\s+/g, "").toLowerCase();
+
+    allReservations.push(data);
+    res.json(data);
 });
 
 app.listen(PORT, function() {
-    console.log(`App listening on PORT: ${PORT} (https://localhost:/${PORT})`);
+    console.log(`App listening on PORT: ${PORT}`);
 })
 
 
